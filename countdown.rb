3385715bd548d203a1718707aed2d31f2be67fc5
Shantel <Shantelgray24@gmail.com>
2025-07-27 20:46:26 -0400

    Loop Lab Practice

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(number = 10)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number, sleep_time = 1)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    sleep(sleep_time)
+    number -= 1
+  end
+end 
